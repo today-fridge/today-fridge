55fee7de5ea679df3c99590a6cd73dc7b944dac1
syg0629 <syg0629@hanmail.net>
2025-09-20 04:29:29 +0900

    chore(TF-49): 의존성 업데이트 및 UI 컴포넌트 수정

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import * as DialogPrimitive from "@radix-ui/react-dialog@1.1.6";
-import { XIcon } from "lucide-react@0.487.0";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
+import { XIcon } from "lucide-react";
 
 import { cn } from "./utils";
 
